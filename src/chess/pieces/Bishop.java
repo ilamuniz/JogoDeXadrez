7691f449eb411c5a0586eda423f05d58fe88c908
ilamuniz <ilammuniz@gmail.com>
2022-07-12 23:31:45 -0300

    Bispo(Bishop)

---
+++ src/chess/pieces/Bishop.java
@@ -0,0 +1,67 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Bishop extends ChessPiece {
+
+    public Bishop(Board board, Color color) {
+        super(board, color);
+    }
+
+    @Override
+    public String toString() {
+        return "B";
+    }
+
+    @Override
+    public boolean[][] possibleMoves() {
+        boolean[][] mat = new boolean[getBoard().getRows()][getBoard().getColumns()];
+
+        Position inicial = new Position(0, 0);
+
+        //noroeste
+        inicial.setValues(position.getRow() - 1, position.getColumn() -1);
+        while (getBoard().positionExists(inicial) && !getBoard().thereIsAPiece(inicial)) {
+            mat[inicial.getRow()][inicial.getColumn()] = true;
+            inicial.setValues(inicial.getRow() -1, inicial.getColumn() - 1);
+        }
+        if (getBoard().positionExists(inicial) && isThereOpponentPiece(inicial)) {
+            mat[inicial.getRow()][inicial.getColumn()] = true;
+        }
+
+        //nordeste
+        inicial.setValues(position.getRow() - 1, position.getColumn() + 1);
+        while (getBoard().positionExists(inicial) && !getBoard().thereIsAPiece(inicial)) {
+            mat[inicial.getRow()][inicial.getColumn()] = true;
+            inicial.setValues(inicial.getRow() - 1, inicial.getColumn() + 1);
+        }
+        if (getBoard().positionExists(inicial) && isThereOpponentPiece(inicial)) {
+            mat[inicial.getRow()][inicial.getColumn()] = true;
+        }
+
+        //sudeste
+        inicial.setValues(position.getRow() + 1, position.getColumn() + 1);
+        while (getBoard().positionExists(inicial) && !getBoard().thereIsAPiece(inicial)) {
+            mat[inicial.getRow()][inicial.getColumn()] = true;
+            inicial.setValues(inicial.getRow() + 1, inicial.getColumn() + 1);
+        }
+        if (getBoard().positionExists(inicial) && isThereOpponentPiece(inicial)) {
+            mat[inicial.getRow()][inicial.getColumn()] = true;
+        }
+
+        //sudoeste
+        inicial.setValues(position.getRow() + 1, position.getColumn() - 1);
+        while (getBoard().positionExists(inicial) && !getBoard().thereIsAPiece(inicial)) {
+            mat[inicial.getRow()][inicial.getColumn()] = true;
+            inicial.setValues(inicial.getRow() + 1, inicial.getColumn() - 1);
+        }
+        if (getBoard().positionExists(inicial) && isThereOpponentPiece(inicial)) {
+            mat[inicial.getRow()][inicial.getColumn()] = true;
+        }
+
+        return mat;
+    }
+}
